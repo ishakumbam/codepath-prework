2eadd6f495e419c28a02bee8f7e3e3cd18690270
ishakumbam <ishakumbam@gmail.com>
2025-04-25 17:07:59 -0500

    Added files prework to repo

---
+++ IOS101-PreworkTests/IOS101_PreworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS101_PreworkTests.swift
+//  IOS101-PreworkTests
+//
+//  Created by Isha Kumbam on 25/04/25.
+//
+
+import Testing
+@testable import IOS101_Prework
+
+struct IOS101_PreworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
